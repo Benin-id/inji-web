667688ee7817bd86425b2b13731515f0e6acf2b4
Mijan <skmijanhossain@gmail.com>
2026-03-14 23:12:08 +0530

    Add

--- inji-web/src/utils/i18n.ts
+++ inji-web/src/utils/i18n.ts
@@ -14,16 +14,10 @@ import {
     LanguageObject
 } from "../types/data";
 
-const resources = {en, ta, kn, hi, fr, ar, pt};
+const resources = {fr};
 
 export const LanguagesSupported: LanguageObject[] = [
-    {label: "English", value: 'en'},
-    {label: "தமிழ்", value: 'ta'},
-    {label: "ಕನ್ನಡ", value: 'kn'},
-    {label: "हिंदी", value: 'hi'},
-    {label: "Français", value: 'fr'},
-    {label: "عربي", value: 'ar'},
-    {label: "Português", value:'pt'}
+    {label: "Français", value: 'fr'}
 ]
 
 export const defaultLanguage = window._env_.DEFAULT_LANG;
